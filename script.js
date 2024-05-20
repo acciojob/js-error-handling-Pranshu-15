45bd7077335bd046f760bc3f2ac99a7bb024a1c8
Pranshu Agrawal <101993746+Pranshu-15@users.noreply.github.com>
2024-05-21 00:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-//your code here
+class OutOfRangeError extends Error {
+    constructor(arg) {
+        super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
+        this.name = 'OutOfRangeError';
+    }
+}
+
+class InvalidExprError extends Error {
+    constructor() {
+        super('Expression should not have an invalid combination of expression');
+        this.name = 'InvalidExprError';
+    }
+}
+
+function evalString(expression) {
+    try {
+        // Remove spaces from the expression
+        expression = expression.replace(/\s+/g, '');
+
+        // Check for invalid characters
+        if (/[^0-9+\-*/]/.test(expression)) {
+            const invalidChar = expression.match(/[^0-9+\-*/]/)[0];
+            throw new OutOfRangeError(invalidChar);
+        }
+
+        // Check for invalid combinations of operators
+        if (/[+\-*/]{2,}/.test(expression)) {
+            throw new InvalidExprError();
+        }
+
+        // Check for invalid starting operator
+        if (/^[+\-*/]/.test(expression)) {
+            throw new SyntaxError('Expression should not start with invalid operator');
+        }
+
+        // Check for invalid ending operator
+        if (/[+\-*/]$/.test(expression)) {
+            throw new SyntaxError('Expression should not end with invalid operator');
+        }
+
+        // Evaluate the expression
+        return eval(expression);
+    } catch (error) {
+        return error.message;
+    }
+}
+
+function evaluateExpression() {
+    const expressionInput = document.getElementById('expression').value;
+    const resultDiv = document.getElementById('result');
+    resultDiv.textContent = evalString(expressionInput);
+}
